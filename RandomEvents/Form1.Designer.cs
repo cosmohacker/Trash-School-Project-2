4e9eb3c6aa8371a6f1359b65966bbbe3b2e07012
Yağızcan Yavuz <yagizcanyavuz@outlook.com>
2020-06-18 00:36:53 +0300

    Add project files.

---
+++ RandomEvents/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace RandomEvents
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDene = new System.Windows.Forms.Button();
+            this.txtTahmin = new System.Windows.Forms.TextBox();
+            this.lblCevap = new System.Windows.Forms.Label();
+            this.lblHak = new System.Windows.Forms.Label();
+            this.lblRandom = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDene
+            // 
+            this.btnDene.Location = new System.Drawing.Point(101, 126);
+            this.btnDene.Name = "btnDene";
+            this.btnDene.Size = new System.Drawing.Size(75, 23);
+            this.btnDene.TabIndex = 0;
+            this.btnDene.Text = "Sorgula";
+            this.btnDene.UseVisualStyleBackColor = true;
+            this.btnDene.Click += new System.EventHandler(this.btnDene_Click);
+            // 
+            // txtTahmin
+            // 
+            this.txtTahmin.Location = new System.Drawing.Point(88, 100);
+            this.txtTahmin.Name = "txtTahmin";
+            this.txtTahmin.Size = new System.Drawing.Size(100, 20);
+            this.txtTahmin.TabIndex = 1;
+            this.txtTahmin.Text = "Rakam";
+            this.txtTahmin.Click += new System.EventHandler(this.txtTahmin_Click);
+            this.txtTahmin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTahmin_KeyPress);
+            // 
+            // lblCevap
+            // 
+            this.lblCevap.AutoSize = true;
+            this.lblCevap.Location = new System.Drawing.Point(98, 162);
+            this.lblCevap.Name = "lblCevap";
+            this.lblCevap.Size = new System.Drawing.Size(0, 13);
+            this.lblCevap.TabIndex = 2;
+            // 
+            // lblHak
+            // 
+            this.lblHak.AutoSize = true;
+            this.lblHak.Location = new System.Drawing.Point(85, 24);
+            this.lblHak.Name = "lblHak";
+            this.lblHak.Size = new System.Drawing.Size(36, 13);
+            this.lblHak.TabIndex = 3;
+            this.lblHak.Text = "Hak : ";
+            this.lblHak.TextChanged += new System.EventHandler(this.lblHak_TextChanged);
+            // 
+            // lblRandom
+            // 
+            this.lblRandom.AutoSize = true;
+            this.lblRandom.Location = new System.Drawing.Point(91, 199);
+            this.lblRandom.Name = "lblRandom";
+            this.lblRandom.Size = new System.Drawing.Size(94, 13);
+            this.lblRandom.TabIndex = 4;
+            this.lblRandom.Text = "Tahmin edilen sayı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblRandom);
+            this.Controls.Add(this.lblHak);
+            this.Controls.Add(this.lblCevap);
+            this.Controls.Add(this.txtTahmin);
+            this.Controls.Add(this.btnDene);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDene;
+        private System.Windows.Forms.TextBox txtTahmin;
+        private System.Windows.Forms.Label lblCevap;
+        private System.Windows.Forms.Label lblHak;
+        private System.Windows.Forms.Label lblRandom;
+    }
+}
+
---
+++ RandomEvents/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomEvents
+{
+    public partial class Form1 : Form
+    {
+        Random random;
+        int num1, hak = 5, tahmin;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            random = new Random();
+            num1 = random.Next(1, 50);
+            lblRandom.Text = num1.ToString();
+        }
+
+        private void btnDene_Click(object sender, EventArgs e)
+        {
+
+            if (txtTahmin.Text.Length == 0)
+            {
+                txtTahmin.Text = "0";
+            }
+
+            if (txtTahmin.Text != num1.ToString())
+            {
+                lblCevap.Text = "Yanlış";
+                hak--;
+                lblHak.Text = "Hak : " + hak.ToString();
+            }
+            else if (txtTahmin.Text == num1.ToString())
+            {
+                lblCevap.Text = "Doğru";
+            }
+
+            if (txtTahmin.Text == "Rakam")
+            {
+                txtTahmin.Text = "0";
+            }
+
+            tahmin = Convert.ToInt32(txtTahmin.Text);
+            if (tahmin < num1)
+            {
+                lblCevap.Text = "Yanlış, Daha büyük olmalı";
+            }
+            else if (tahmin > num1)
+            {
+                lblCevap.Text = "Yanlış, Daha küçük olmalı";
+            }
+
+            if (txtTahmin.Text == num1.ToString())
+            {
+                lblCevap.Text = "Doğru";
+                lblHak.Text = "Hak : " + hak.ToString();
+                MessageBox.Show("Tebrikler başardın " + num1.ToString(), "You Survived", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Dispose();
+            }
+        }
+
+        private void lblHak_TextChanged(object sender, EventArgs e)
+        {
+            if (lblHak.Text == "Hak : 0")
+            {
+                MessageBox.Show("Sayıyı bulamadınız tutulan sayı " + num1.ToString(), "You Died", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txtTahmin_Click(object sender, EventArgs e)
+        {
+            if (txtTahmin.Text == "Rakam")
+            {
+                txtTahmin.Text = "";
+            }
+        }
+
+        private void txtTahmin_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+
+        }
+    }
+}
